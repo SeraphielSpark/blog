68b2422f4929557263635a6313e9e52e7b3d2dfc
SeraphielSpark <taiwoemmanuel435@gmail.com>
2025-08-14 19:40:03 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Initialize database first
-python -c "
-from app import app
-from models import db
-
-with app.app_context():
-    db.create_all()
-"
-
 # Start Gunicorn
 gunicorn --bind 0.0.0.0:$PORT app:app
