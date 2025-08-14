4618cbffd56afb9c9e96e6f32bfe399c2f393a24
SeraphielSpark <taiwoemmanuel435@gmail.com>
2025-08-14 20:04:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/env bash
-set -o errexit
-
 pip install -r requirements.txt
-
-# Initialize database
-python -c "
-from app import app, db, User, Post, Comment
-
-with app.app_context():
-    db.create_all()
-    if not User.query.filter_by(username='admin').first():
-        admin = User(username='admin', online=True)
-        admin.set_password('admin123')
-        db.session.add(admin)
-        db.session.commit()
-"
