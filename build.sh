ea4974147cde5e994aa2726719f29927677454ff
SeraphielSpark <taiwoemmanuel435@gmail.com>
2025-08-14 19:11:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-flask db init
-flask db migrate
-flask db upgrade
+
+# Initialize database
+python -c "
+from app import app
+from models import db, User, Post, Comment
+
+with app.app_context():
+    db.create_all()
+    if not User.query.filter_by(username='admin').first():
+        admin = User(username='admin', online=True)
+        admin.set_password('admin123')
+        db.session.add(admin)
+        db.session.commit()
+"
